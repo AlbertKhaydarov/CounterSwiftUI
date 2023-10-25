825fe18c507992fe753f34015eb883d45bc509f5
AlbertKhaydarov <aghaydarov@gmail.com>
2023-10-25 22:39:27 +0300

    Initial Commit

---
+++ CounterSwiftUI/CounterSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CounterSwiftUIApp.swift
+//  CounterSwiftUI
+//
+//  Created by Альберт Хайдаров on 25.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct CounterSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
