b0bc9ddfadb616c8f267d6f257db552c645b6569
AlbertKhaydarov <aghaydarov@gmail.com>
2023-10-26 16:13:16 +0300

    add  layout

--- CounterSwiftUI/ContentView.swift
+++ CounterSwiftUI/ContentView.swift
@@ -8,14 +8,57 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var tapCount: Int = 0
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        
+        Color.pink
+            .ignoresSafeArea()
+            .overlay (
+                VStack {
+                    
+                    Text("Счетчик")
+                        .font(.system(size: 32))
+                        .foregroundColor(.white)
+                        .padding(.top, 0.0)
+        
+                    
+                    Button(action: {
+                        tapCount += 1
+                    })  {
+                        Text("Значение счетчика \(tapCount)")
+                        
+                            .font(.system(size: 24))
+                            .foregroundColor(.white)
+                            .padding(.top, 169.0)
+                    }
+                    .padding(50)
+                    HStack() {
+                        Button (action: {
+                            print("+")
+                        }, label: {
+                            Image("plus32")
+                                .frame(width: 100.0, height: 100.0)
+                              
+//                                .foregroundColor(.green)
+                        })
+                      
+                                            
+                                            .scaledToFill()
+//                        .padding(0.0)
+//                        .buttonStyle(PlainButtonStyle())
+//                        .foregroundColor(Color(red: 250, green: 147, blue: 150))
+                        .foregroundColor(.white)
+                        .clipShape(Circle())
+//                        .buttonBorderShape(.capsule)
+                        .contentShape(Circle())
+
+                    }
+                    .padding()
+                    
+                    
+                })
+        
     }
 }
 
